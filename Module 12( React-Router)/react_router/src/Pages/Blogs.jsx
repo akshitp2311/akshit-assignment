f4c33165556f702d9e1a7621762b9ff83d923eae
akshitp2311 <akshitp2306@gmail.com>
2023-10-20 00:07:18 +0530

    19-10

---
+++ Module 12( React-Router)/react_router/src/Pages/Blogs.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Blogs() {
+  return (
+    <>
+    <h1>
+    Blogs
+    </h1>
+    
+    </>
+  )
+}
---
+++ Module 12( React-Router)/react_router/src/Pages/Layout.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom'
+import Blogs from './Blogs'
+import Contact from './Contact'
+import Home from './Home'
+import NoPage from './NoPage'
+
+export default function Layout() {
+  return (
+    <>
+    <BrowserRouter>
+        <ul>
+            <li>
+                <NavLink to={"/"}>Home</NavLink>
+            </li>
+            <li>
+                <NavLink to={"/blogs"}>Blogs</NavLink>
+            </li>
+            <li>
+                <NavLink to={"/contact"}>Contact</NavLink>
+            </li>
+        </ul>
+        <Routes>
+            <Route exact path={"/"} element={<Home />} />
+            <Route exact path={"/blogs"} element={<Blogs />} />
+            <Route exact path={"/contact"} element={<Contact/>} />
+            <Route exact path={"/404"} element={<NoPage />} />
+        </Routes>
+    </BrowserRouter>
+    </>
+  )
+}
