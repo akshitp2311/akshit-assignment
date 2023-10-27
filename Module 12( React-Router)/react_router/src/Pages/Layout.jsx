bff67983dc9e983c5209addf2a928ac4636a7b56
akshitp2311 <akshitp2306@gmail.com>
2023-10-27 09:34:11 +0530

    node

--- Module 12( React-Router)/react_router/src/Pages/Layout.jsx
+++ Module 12( React-Router)/react_router/src/Pages/Layout.jsx
@@ -6,27 +6,29 @@ import Home from './Home'
 import NoPage from './NoPage'
 
 export default function Layout() {
-  return (
-    <>
-    <BrowserRouter>
-        <ul>
-            <li>
-                <NavLink to={"/"}>Home</NavLink>
-            </li>
-            <li>
-                <NavLink to={"/blogs"}>Blogs</NavLink>
-            </li>
-            <li>
-                <NavLink to={"/contact"}>Contact</NavLink>
-            </li>
-        </ul>
-        <Routes>
-            <Route exact path={"/"} element={<Home />} />
-            <Route exact path={"/blogs"} element={<Blogs />} />
-            <Route exact path={"/contact"} element={<Contact/>} />
-            <Route exact path={"/404"} element={<NoPage />} />
-        </Routes>
-    </BrowserRouter>
-    </>
-  )
+    return (
+        <>
+            <div className='display-flex flex-1 '>
+                <BrowserRouter>
+                    <ul>
+                        <li>
+                            <NavLink to={"/"}>Home</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to={"/blogs"}>Blogs</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to={"/contact"}>Contact</NavLink>
+                        </li>
+                    </ul>
+                    <Routes>
+                        <Route exact path={"/"} element={<Home />} />
+                        <Route exact path={"/blogs"} element={<Blogs />} />
+                        <Route exact path={"/contact"} element={<Contact />} />
+                        <Route exact path={"/404"} element={<NoPage />} />
+                    </Routes>
+                </BrowserRouter>
+            </div>
+        </>
+    )
 }
